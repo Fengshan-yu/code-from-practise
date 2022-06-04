c5bd33b001ce2b34078c5b456e694b567bd2dde9
Fengshan Yu <95190262+Fengshan-yu@users.noreply.github.com>
2022-06-03 21:22:45 -0400

    Add files via upload

--- decipher.py
+++ decipher.py
@@ -1,12 +1,4 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Mar  5 16:04:02 2022
-
-@author: adminstrator
-"""
-
-
-#Caesar cipher improved
+# Caesar cipher improved
 
 message = input("Enter a message: ")
 shift_value = int(input("Enter a number between 1 to 25: "))
@@ -16,23 +8,21 @@ if shift_value < 1:
 elif shift_value > 25:
     print("shift_value too high.")
 
-cipher = ""   
+cipher = ""
 for char in message:
     if not char.isalpha():
-        code = chr(ord(char))
+        code = char
     elif char.islower():
-        if ord(char)+shift_value>=123:
-            code = chr(97+(ord(char)+shift_value-123))
-        else:    
-            code = chr(ord(char)+shift_value)
-    elif char.isupper():
-        if ord(char)+shift_value>=91:
-            code = chr(65+(ord(char)+shift_value-91))
+        if ord(char) + shift_value >= 123:
+            code = chr(97 + (ord(char) + shift_value - 123))
         else:
-            code = chr(ord(char)+shift_value)
-            
-    cipher+=code
-    
-print(cipher)
+            code = chr(ord(char) + shift_value)
+    elif char.isupper():
+        if ord(char) + shift_value >= 91:
+            code = chr(65 + (ord(char) + shift_value - 91))
+        else:
+            code = chr(ord(char) + shift_value)
 
-    
+    cipher += code
+
+print(cipher)
